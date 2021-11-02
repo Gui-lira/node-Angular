226e0f18f785a774646a9f79e4109dda084c41fe
Gui-lira <gui.lira.martins@gmail.com>
2021-11-02 20:38:27 -0300

    register component

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+import axios from 'axios';
 
 @Component({
   selector: 'app-register',
@@ -11,13 +12,26 @@ export class RegisterComponent {
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
-  ) { }
+  ) { };
   
   registerFrom = this.formBuilder.group({
     email: '',
     password: '',
     cpf: '',
     name: '',
-  })
+  });
+
+  private redirect(): void {
+    this.router.navigate(['rentalCars']);
+  };
+
+  async onSubmit(): Promise<void> {
+    const { value } = this.registerFrom;
+    const raw = await axios.post('http://localhost:3000/user');
+    if (raw.status === 200) {
+      localStorage.setItem('token', raw.data.token);
+      this.redirect();
+    }
+  }
 
 }
